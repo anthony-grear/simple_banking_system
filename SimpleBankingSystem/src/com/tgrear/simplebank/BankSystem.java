dbc149eb1bf7395dea8d2ccf2be51d778c1cfc07
Anthony Grear <tgrear2008@gmail.com>
2022-01-10 22:11:35 +0700

    Store account numbers and pins in a HashMap

--- SimpleBankingSystem/src/com/tgrear/simplebank/BankSystem.java
+++ SimpleBankingSystem/src/com/tgrear/simplebank/BankSystem.java
@@ -1,7 +1,10 @@
 package com.tgrear.simplebank;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 class BankSystem {
+	static Map<String, String> accountMap = new HashMap<String, String>();
 	
 	private static void displayMainMenu() {
 		System.out.println("1. Create an account");
@@ -12,14 +15,16 @@ class BankSystem {
 	private static String generateAccountNumber() {
 		Random random = new Random();
 		String[] randomIntArr = new String[10];
-		for (int i = 0; i < 10; i++) {
-			randomIntArr[i] = String.valueOf(random.nextInt(10));			
-		}
 		String accountNumber = "400000";
-		for (int i = 0; i < 10; i++) {
-			accountNumber +=randomIntArr[i];
-		}
-		
+		do {			
+			for (int i = 0; i < 10; i++) {
+				randomIntArr[i] = String.valueOf(random.nextInt(10));			
+			}			
+			for (int i = 0; i < 10; i++) {
+				accountNumber +=randomIntArr[i];
+			}			
+		} while (accountMap.containsKey(accountNumber));
+		accountMap.put(accountNumber, generatePinNumber());		
 		return accountNumber;
 	}
 	
@@ -38,11 +43,13 @@ class BankSystem {
 	}
 	
 	private static void displayAccountCreation() {
+		String accountNumberOutput;
 		System.out.println("Your card has been created");
 		System.out.println("Your card number:");
-		System.out.println(generateAccountNumber());
+		accountNumberOutput = generateAccountNumber();
+		System.out.println(accountNumberOutput);
 		System.out.println("Your card PIN:");
-		System.out.println(generatePinNumber());
+		System.out.println(accountMap.get(accountNumberOutput));
 	}
 	
 	private static void successfulLoginMenu() {
@@ -56,8 +63,8 @@ class BankSystem {
 //		displayMainMenu();
 //		System.out.println(generateAccountNumber());
 //		System.out.println(generatePinNumber());
-//		displayAccountCreation();
-		successfulLoginMenu();
+		displayAccountCreation();
+//		successfulLoginMenu();
 
 	}
 
