e4900c66376e33d24fa16c11924649abcd7b08fc
Anthony Grear <tgrear2008@gmail.com>
2022-01-10 16:34:08 +0700

    Add method generatePinNumber(). returns string of 4-digit pin.

--- SimpleBankingSystem/src/com/tgrear/simplebank/BankSystem.java
+++ SimpleBankingSystem/src/com/tgrear/simplebank/BankSystem.java
@@ -15,17 +15,32 @@ class BankSystem {
 		for (int i = 0; i < 10; i++) {
 			randomIntArr[i] = String.valueOf(random.nextInt(10));			
 		}
-		String randomNumberAsString = "400000";
+		String accountNumber = "400000";
 		for (int i = 0; i < 10; i++) {
-			randomNumberAsString +=randomIntArr[i];
+			accountNumber +=randomIntArr[i];
 		}
 		
-		return randomNumberAsString;
+		return accountNumber;
+	}
+	
+	private static String generatePinNumber() {
+		Random random = new Random();
+		String[] randomIntArr = new String[4];
+		for (int i = 0; i < 4; i++) {
+			randomIntArr[i] = String.valueOf(random.nextInt(10));			
+		}
+		String pinNumber = "";
+		for (int i = 0; i < 4; i++) {
+			pinNumber +=randomIntArr[i];
+		}
+		
+		return pinNumber;
 	}
 	
 	public static void main(String[] args) {
-//		displayMainMenu();
+		displayMainMenu();
 		System.out.println(generateAccountNumber());
+		System.out.println(generatePinNumber());
 
 	}
 
